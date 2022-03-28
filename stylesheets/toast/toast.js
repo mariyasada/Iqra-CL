743f59846561efc7d2a8dbb8cc2b0e4c2f24f32d
mariyasada <mariyasada2406@gmail.com>
2022-03-28 12:05:00 +0530

    doc: changing css of toast

--- stylesheets/toast/toast.js
+++ stylesheets/toast/toast.js
@@ -2,6 +2,7 @@ const toastCodeBtn = document.querySelector("#toast-code-btn");
 const toastCodeDiv = document.querySelector("#toast-div-code");
 const toastClickMeBtn =document.querySelector("#toast-button");
 const toastDiv=document.querySelector("#toast-bottom");
+const snackbar = document.querySelector(".bg-with-shadow");
 
       toastCodeBtn.addEventListener("click", () => {
         
@@ -10,10 +11,11 @@ const toastDiv=document.querySelector("#toast-bottom");
           : (toastCodeDiv.style.display = "block");
       });
 
-      toastClickMeBtn.addEventListener("click",()=>{       
-          toastDiv.style.display==="none"?  toastDiv.style.display="block": toastDiv.style.display="none";
-      })
+      toastClickMeBtn.addEventListener("click",()=>{    
+          toastDiv.className="show";
+          setTimeout(()=>{
+                 toastDiv.className= toastDiv.className.replace("show","");
+          },2000)
+        })
 
-      toastDiv.addEventListener("click",()=>{
-           toastDiv.style.display==="block"?  toastDiv.style.display="none": toastDiv.style.display="block";
-      })
+      
